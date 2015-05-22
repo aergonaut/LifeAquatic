5631f72ca78f4ac855607a551d6170a86472f0d8
Chris Fung <aergonaut@gmail.com>
2015-05-21 18:11:18 -0700

    switch to scala

---
+++ src/main/scala/com/aergonaut/lifeaquatic/LifeAquatic.scala
@@ -0,0 +1,8 @@
+package com.aergonaut.lifeaquatic
+
+import cpw.mods.fml.common.Mod
+
+@Mod(name = "Life Aquatic", version = "1.7.10-0.0.1", modid = "lifequatic", modLanguage = "scala")
+object LifeAquatic {
+
+}
