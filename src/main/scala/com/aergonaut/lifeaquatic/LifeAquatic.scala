9263a188a9544c111c0be7766ac17020df6d1b9b
Chris Fung <aergonaut@gmail.com>
2015-05-21 20:50:24 -0700

    add some logging and clean up the mod props

--- src/main/scala/com/aergonaut/lifeaquatic/LifeAquatic.scala
+++ src/main/scala/com/aergonaut/lifeaquatic/LifeAquatic.scala
@@ -1,8 +1,30 @@
 package com.aergonaut.lifeaquatic
 
 import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.Mod.EventHandler
+import cpw.mods.fml.common.event.{FMLPostInitializationEvent, FMLInitializationEvent, FMLPreInitializationEvent}
+import org.apache.logging.log4j.{Level, Logger}
 
-@Mod(name = "Life Aquatic", version = "1.7.10-0.0.1", modid = "lifequatic", modLanguage = "scala")
+@Mod(name = LifeAquatic.name, version = LifeAquatic.version, modid = LifeAquatic.modid, modLanguage = "scala")
 object LifeAquatic {
+  final val name = "Life Aquatic"
+  final val version = "1.7.10-0.0.1"
+  final val modid = "lifeaquatic"
 
+  var logger: Logger = null
+
+  @EventHandler
+  def preInit(event: FMLPreInitializationEvent): Unit = {
+    logger = event.getModLog
+  }
+
+  @EventHandler
+  def init(event: FMLInitializationEvent): Unit = {
+
+  }
+
+  @EventHandler
+  def postInit(event: FMLPostInitializationEvent): Unit = {
+    logger.log(Level.INFO, s"${LifeAquatic.name} has loaded successfully.")
+  }
 }
