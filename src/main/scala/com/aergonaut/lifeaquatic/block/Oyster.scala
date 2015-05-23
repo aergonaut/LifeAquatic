87662dbd444446aa429cb12cd40848bbf157c61b
Chris Fung <aergonaut@gmail.com>
2015-05-23 16:41:29 -0700

    oysters drop pearls

--- src/main/scala/com/aergonaut/lifeaquatic/block/Oyster.scala
+++ src/main/scala/com/aergonaut/lifeaquatic/block/Oyster.scala
@@ -1,11 +1,21 @@
 package com.aergonaut.lifeaquatic.block
 
+import java.util
+
+import com.aergonaut.lifeaquatic.item.ModItems
 import cpw.mods.fml.relauncher.{Side, SideOnly}
 import net.minecraft.client.renderer.texture.IIconRegister
+import net.minecraft.item.ItemStack
 import net.minecraft.util.IIcon
+import net.minecraft.world.World
 
 class Oyster extends BlockBase("Oyster") {
+  setHardness(3.0F)
+  setResistance(5.0F)
+
   var iconSide, iconTop: IIcon = _
+
+  val MAX_PEARLS = 5
 
   override def registerBlockIcons(iIconRegister: IIconRegister): Unit = {
     iconTop = iIconRegister.registerIcon(getUnlocalizedName().split('.').last + ".0")
@@ -17,5 +27,12 @@ class Oyster extends BlockBase("Oyster") {
     case 0 => iconTop
     case 1 => iconTop
     case _ => iconSide
+  }
+
+  override def getDrops(world: World, x: Int, y: Int, z: Int, metadata: Int, fortune: Int): util.ArrayList[ItemStack] = {
+    val count = Math.max(world.rand.nextInt(fortune + MAX_PEARLS), MAX_PEARLS)
+    val array = new util.ArrayList[ItemStack]()
+    array.add(new ItemStack(ModItems.pearl, count))
+    array
   }
 }
