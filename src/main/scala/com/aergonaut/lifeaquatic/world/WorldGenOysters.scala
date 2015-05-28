2bf234892229888167209d0e465fda9256043e8c
Chris Fung <aergonaut@gmail.com>
2015-05-28 13:32:48 -0700

    remove logging

--- src/main/scala/com/aergonaut/lifeaquatic/world/WorldGenOysters.scala
+++ src/main/scala/com/aergonaut/lifeaquatic/world/WorldGenOysters.scala
@@ -3,13 +3,11 @@ package com.aergonaut.lifeaquatic.world
 import java.util.Random
 
 import com.aergonaut.lifeaquatic.block.ModBlocks
-import com.aergonaut.lifeaquatic.util.Logger
 import net.minecraft.world.World
 import net.minecraft.world.gen.feature.WorldGenerator
 
 class WorldGenOysters extends WorldGenerator {
   override def generate(world: World, random: Random, x: Int, y: Int, z: Int): Boolean = {
-    Logger.info(s"Generating an oyster at (${x}, ${y}, ${z})")
     world.setBlock(x, y, z, ModBlocks.Oyster)
   }
 }
