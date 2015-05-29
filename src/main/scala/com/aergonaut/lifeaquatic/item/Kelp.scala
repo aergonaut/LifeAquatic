0285f48cba58457864c2ab54e827e3bdd9dec119
Ian McNelly <ian.mcnelly@coupa.com>
2015-05-29 08:43:40 -0400

    Cleanup for ScalaStyle

--- src/main/scala/com/aergonaut/lifeaquatic/item/Kelp.scala
+++ src/main/scala/com/aergonaut/lifeaquatic/item/Kelp.scala
@@ -4,7 +4,5 @@ import com.aergonaut.lifeaquatic.constants.Names
 /**
  * Created by ianmcnelly on 5/29/15.
  */
-class Kelp extends ItemBase(Names.Items.Kelp){
-
-}
+class Kelp extends ItemBase(Names.Items.Kelp)
 
